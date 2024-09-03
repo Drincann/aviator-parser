9a25134b997463f1f3d7041b46f23f67561c2c47
gaojunkang <gaojunkang@xiaohongshu.com>
2024-09-03 23:16:44 +0800

    feat: support ts typings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { Lexer } from './lexer.js'
 export { AviatorParser } from './parser.js'
-export type { ExpressionNode, StringLiteral, NumberLiteral, BinaryOperator, Identifier } from './parser.js'
+export type {
+  ExpressionNode, StringLiteral, NumberLiteral, BinaryOperator, Identifier, BooleanLiteral,
+  FunctionCall, NilLiteral, RegexLiteral, StatementNode, UnaryOperator
+} from './parser.js'
 export type { Token, TokenType } from './lexer.js'
